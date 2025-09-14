ff79be2ea6f08b3132bd5a85644748b1ea9f6fb4
azadengineer38-spec <azadengineer38@gmail.com>
2025-09-14 19:56:24 +0530

    Update apiconfig.js

--- src/config/apiconfig.js
+++ src/config/apiconfig.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // export const API_BASE_URL="http://localhost:8485"
-export const API_BASE_URL="https://diligent-solace-production.up.railway.app"
+//export const API_BASE_URL="https://diligent-solace-production.up.railway.app"
+export const API_BASE_URL="https://dockerrenderrail-latest.onrender.com"
+
 
 
 let jwt=localStorage.getItem("jwt");
